23dc762e91f1bd3ac7cd6f7437ebda9ccadf4372
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-23 06:53:09 -0600

    fix sidechain url

--- src/webaverse/constants.js
+++ src/webaverse/constants.js
@@ -71,4 +71,4 @@ export const contractsHost = 'https://contracts.webaverse.com';
 export const presenceHost = 'worlds.webaverse.com';
 export const localstorageHost = 'https://localstorage.webaverse.com';
 export const loginEndpoint = 'https://login.exokit.org';
-export const web3SidechainEndpoint = 'https://ethereum.exokit.org';
+export const web3SidechainEndpoint = 'https://ethereum.exokit.org:8545';
